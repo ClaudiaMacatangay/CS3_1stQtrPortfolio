309be748f78623e2d81ef7294c92749547f9023c
ClaudiaMacatangay <b2029cmdlmacatangay@pshs.edu.ph>
2025-08-05 09:06:12 +0800

    commit 1ng

--- public/script.js
+++ public/script.js
@@ -2,5 +2,5 @@
 var age = 14
 var maxAge = 10000
 var numPerDay = 5
-totalRequred = (numPerDay*365)*(maxAge-age)
-document.write("You will need" +totalRequired+ "to last you until the ripe old age of" +maxAge)
+totalRequired = (numPerDay*365)*(maxAge-age)
+document.write("You will need " +totalRequired+ " to last you until the ripe old age of " +maxAge)
